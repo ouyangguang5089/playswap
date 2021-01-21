1209e9ecc43aa3420df51135ab2a4f8bd0050e17
ouyangguang <508925634@qq.com>
2021-01-21 17:37:58 +0800

    first

---
+++ node_modules/@ethersproject/experimental/lib.esm/index.js
@@ -0,0 +1,4 @@
+"use strict";
+import { NonceManager } from "./nonce-manager";
+export { NonceManager };
+//# sourceMappingURL=index.js.map
---
+++ node_modules/@ethersproject/experimental/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var nonce_manager_1 = require("./nonce-manager");
+exports.NonceManager = nonce_manager_1.NonceManager;
+//# sourceMappingURL=index.js.map
